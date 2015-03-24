865cb59716f3275f543916eac33faae5715b9c8b
Sean Tsai <kidd826@gmail.com>
2015-03-24 15:39:08 +0800

    Ruby flow controrol ex5

--- flow_control:ex3.rb
+++ flow_control:ex3.rb
@@ -1,12 +1,27 @@
 puts "請輸入一個 0 至 100 的數值"
 number = gets.chomp.to_i
 
-  if number < 0
-    puts "#{number} 不在 0 至 100 之間啦"
-  elsif number <= 50
-    puts "#{number} is between 0 and 50"
-  elsif number <=100
-    puts "#{number} is between 51 and 100"
-  else
-    puts "#{number} 超過 100 了啦"
+  # if number < 0
+    # puts "#{number} 不在 0 至 100 之間啦"
+  # elsif number <= 50
+    # puts "#{number} is between 0 and 50"
+  # elsif number <=100
+    # puts "#{number} is between 51 and 100"
+  # else
+    # puts "#{number} 超過 100 了啦"
+  # end
+
+def eat_num(number)
+  case
+    when number < 0
+      puts "#{number} 不在 0 至 100 之間啦"
+    when number <= 50
+      puts "#{number} is between 0 and 50"
+    when number <=100
+      puts "#{number} is between 51 and 100"
+    else
+      puts "#{number} 超過 100 了啦"
   end
+end
+
+eat_num(number)
