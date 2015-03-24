9663bd539da5df318f55c5e41d934867617899be
Sean Tsai <kidd826@gmail.com>
2015-03-24 16:25:44 +0800

    Ruby loop iterators ex3

---
+++ loops_iterators:ex3.rb
@@ -0,0 +1,9 @@
+caribbean_pirates = ["Jack Sparrow",
+       "Will Turner",
+       "Elizabeth Swann",
+       "Davy Jones",
+       "Hector Barbossa"]
+
+caribbean_pirates.each_with_index do | pirates, index |
+  puts "#{index}. #{pirates}"
+end
